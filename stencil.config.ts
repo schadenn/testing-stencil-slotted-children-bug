c92f30d6cf2759f8dad0afa7b8a1a8bb1177d2c6
Schaller <EH2S1O2@ztb.icb.commerzbank.com>
2019-07-11 09:15:00 +0200

    Removed unnecessary stuff

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,15 +1,10 @@
 import { Config } from '@stencil/core';
-import { tss } from 'stencil-plugin-tss';
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
-  plugins: [
-    tss()
-  ],
-  excludeSrc: ["*.styles.ts"],
   outputTargets: [
     {
       type: 'www',
